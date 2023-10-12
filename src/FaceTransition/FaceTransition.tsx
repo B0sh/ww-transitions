29cfffc7d73ff47627991fc604968e12fb4b574e
B0sh <b0shbbl@hotmail.com>
2023-10-11 19:53:00 -0500

    Formatting

--- src/FaceTransition/FaceTransition.tsx
+++ src/FaceTransition/FaceTransition.tsx
@@ -1,55 +1,70 @@
-import {AbsoluteFill, interpolate, spring, useCurrentFrame, useVideoConfig} from 'remotion';
-import { FaceImage } from './FaceImage';
+import {
+    AbsoluteFill,
+    interpolate,
+    spring,
+    useCurrentFrame,
+    useVideoConfig,
+} from "remotion";
+import { FaceImage } from "./FaceImage";
 
 type Props = {
-  faces: string[];
-}
+    faces: string[];
+};
 
 export const FaceTransition: React.FC<Props> = ({ faces }) => {
-  const { fps, durationInFrames, width, height } = useVideoConfig();
+    const { fps, durationInFrames, width, height } = useVideoConfig();
 
-  const frame = useCurrentFrame();
-  const progress = spring({
-    fps: fps,
-    frame,
-    durationInFrames: durationInFrames,
-    config: {
-      damping: 2,
-      stiffness: 0.8,
-    },
-  });
+    const frame = useCurrentFrame();
+    const progress = spring({
+        fps: fps,
+        frame,
+        durationInFrames: durationInFrames,
+        config: {
+            damping: 2,
+            stiffness: 0.8,
+        },
+    });
 
-  // Translate the purple screen from the left, transitioning in 1 second (30 frames at 30fps)
-  const faceSize = height / faces.length;
-  const translateX = interpolate(progress, [0, 1], [-1920 - faceSize * 2, 0]);
+    // Translate the purple screen from the left, transitioning in 1 second (30 frames at 30fps)
+    const faceSize = height / faces.length;
+    const translateX = interpolate(progress, [0, 1], [-1920 - faceSize * 2, 0]);
 
-  return (
-    <AbsoluteFill style={{backgroundColor: '#00FF00'}}>
-      <div style={{
-        top: 0,
-        left: `-${width}px`,
-        right: 0,
-        bottom: 0,
-        width: width + faceSize,
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
+    return (
+        <AbsoluteFill style={{ backgroundColor: "#00FF00" }}>
+            <div
+                style={{
+                    top: 0,
+                    left: `-${width}px`,
+                    right: 0,
+                    bottom: 0,
+                    width: width + faceSize,
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
 
-        backgroundColor: '#FFFF00',
-        transform: `translateX(${translateX}px)`,
-      }}>
-
-        <div style={{
-          position: 'relative',
-          left: `${width + faceSize}px`,
-          height: `${height}px`
-        }}>
-          {faces.map((face, i) => {
-            return <FaceImage key={i} face={face} size={faceSize} index={i}/>
-          })}
-        </div>
-
-      </div>
-    </AbsoluteFill>
-  );
+                    backgroundColor: "#FFFF00",
+                    transform: `translateX(${translateX}px)`,
+                }}
+            >
+                <div
+                    style={{
+                        position: "relative",
+                        left: `${width + faceSize}px`,
+                        height: `${height}px`,
+                    }}
+                >
+                    {faces.map((face, i) => {
+                        return (
+                            <FaceImage
+                                key={i}
+                                face={face}
+                                size={faceSize}
+                                index={i}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+        </AbsoluteFill>
+    );
 };
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,26 +1,20 @@
-import {Composition} from 'remotion';
-import { FaceTransition } from './FaceTransition/FaceTransition';
+import { Composition } from "remotion";
+import { FaceTransition } from "./FaceTransition/FaceTransition";
 
 // Each <Composition> is an entry in the sidebar!
 
 export const RemotionRoot: React.FC = () => {
-  return (
-    <Composition
-      id="MyVideo"
-      component={FaceTransition}
-      durationInFrames={60}
-      fps={30}
-      width={1920}
-      height={1080}
-			defaultProps={{
-				faces: [
-					'B0sh',
-					'Elwyn',
-					'Farynheight',
-					'B0sh',
-					'Elwyn',
-				]
-			}}
-    />
-  );
+    return (
+        <Composition
+            id="MyVideo"
+            component={FaceTransition}
+            durationInFrames={60}
+            fps={30}
+            width={1920}
+            height={1080}
+            defaultProps={{
+                faces: ["B0sh", "Elwyn", "Farynheight", "B0sh", "Elwyn"],
+            }}
+        />
+    );
 };
