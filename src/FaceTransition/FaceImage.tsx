6f8c3d8271c465f1db693ad0e74183a256ed526e
B0sh <b0shbbl@hotmail.com>
2023-10-11 19:44:15 -0500

    WW Face Transition

---
+++ src/FaceTransition/FaceImage.tsx
@@ -0,0 +1,26 @@
+import { Img, staticFile } from "remotion";
+
+interface Props {
+  face: string;
+  size: number;
+  index: number;
+}
+
+export const FaceImage: React.FC<Props> = ({ face, size, index }) => {
+
+  // const { fps, durationInFrames, width, height } = useVideoConfig();
+  // const frame = useCurrentFrame();
+
+  return (
+      <div style={{
+        position: 'relative',
+        // background: 'blue',
+        width: `${size}px`,
+        height: `${size}px`,
+        // border: '1px solid black',
+        left: (index % 2) == 1 ? `-${size}px` : '0',
+      }}>
+        <Img src={staticFile(`${face}-face.png`)} width={`${size}px`} height={`${size}px`} />
+      </div>
+  );
+}
